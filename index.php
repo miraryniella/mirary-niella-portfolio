21222a2d01995293d37557fc5aa0f1f11b55ee95
miraryniella <niellamirary@gmail.com>
2024-06-21 15:20:58 +0300

    'need a html file'

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo 'hello word, welcome to mirary.niella.portfolio';
-
-// require(__DIR__.'/assets/templates/_header.php');
-// require(__DIR__.'/assets/templates/_body.php');
-// require(__DIR__.'/assets/templates/_footer.php');
-
-
